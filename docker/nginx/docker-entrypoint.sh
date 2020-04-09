a9be22906184cfc1381418402fc6671f6397dfb8
Wiryono Lauw <wiryonolau@gmail.com>
2020-04-09 16:24:15 +0700

    Fix temporary cert

--- docker/nginx/docker-entrypoint.sh
+++ docker/nginx/docker-entrypoint.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 mkdir -p ${CERT_PATH}
 
-if [ -z "${WORDPRESS_CERT_FILE}" ]; then
-    openssl rand -base64 32 > ${CERT_PATH}/wp_passphrase.txt
-    openssl genrsa -aes256 -passout file:${CERT_PATH}/wp_passphrase.txt -out ${CERT_PATH}/wp.key 2048
-    openssl req -new -passin file:${CERT_PATH}/wp_passphrase.txt -key ${CERT_PATH}/wp.key -out ${CERT_PATH}/wp.csr -subj "/C=ID/O=DEV/OU=IT/CN=${WORDPRESS_SERVER_NAME}"
-    cp ${CERT_PATH}/wp.key ${CERT_PATH}/wp.key.org
-    openssl rsa -in ${CERT_PATH}/wp.key.org -passin file:${CERT_PATH}/wp_passphrase.txt -out ${CERT_PATH}/wp.key
-    openssl x509 -req -days 3650 -in ${CERT_PATH}/wp.csr -signkey ${CERT_PATH}/wp.key -out ${CERT_PATH}/wp.crt
-    export WORDPRESS_CERT_FILE="${CERT_PATH}/wp.crt"
-    export WORDPRESS_CERT_KEY_FILE="${CERT_PATH}/wp.key"
+if [ ! -f "${WORDPRESS_CERT_FILE}" ]; then
+    if [ ! -w "${CERT_PATH}" ]; then 
+        WORDPRESS_CERT_PATH=/tmp
+    else
+        WORDPRESS_CERT_PATH=${CERT_PATH}
+    fi
+    openssl rand -base64 32 > ${WORDPRESS_CERT_PATH}/wp_passphrase.txt
+    openssl genrsa -aes256 -passout file:${WORDPRESS_CERT_PATH}/wp_passphrase.txt -out ${WORDPRESS_CERT_PATH}/wp.key 2048
+    openssl req -new -passin file:${WORDPRESS_CERT_PATH}/wp_passphrase.txt -key ${WORDPRESS_CERT_PATH}/wp.key -out ${WORDPRESS_CERT_PATH}/wp.csr -subj "/C=ID/O=DEV/OU=IT/CN=${WORDPRESS_SERVER_NAME}"
+    cp ${WORDPRESS_CERT_PATH}/wp.key ${WORDPRESS_CERT_PATH}/wp.key.org
+    openssl rsa -in ${WORDPRESS_CERT_PATH}/wp.key.org -passin file:${WORDPRESS_CERT_PATH}/wp_passphrase.txt -out ${WORDPRESS_CERT_PATH}/wp.key
+    openssl x509 -req -days 3650 -in ${WORDPRESS_CERT_PATH}/wp.csr -signkey ${WORDPRESS_CERT_PATH}/wp.key -out ${WORDPRESS_CERT_PATH}/wp.crt
+    export WORDPRESS_CERT_FILE="${WORDPRESS_CERT_PATH}/wp.crt"
+    export WORDPRESS_CERT_KEY_FILE="${WORDPRESS_CERT_PATH}/wp.key"
 fi
 
-if [ -z "${WHMCS_CERT_FILE}" ]; then
-    openssl rand -base64 32 > ${CERT_PATH}/whmcs_passphrase.txt
-    openssl genrsa -aes256 -passout file:${CERT_PATH}/whmcs_passphrase.txt -out ${CERT_PATH}/whmcs.key 2048
-    openssl req -new -passin file:${CERT_PATH}/whmcs_passphrase.txt -key ${CERT_PATH}/whmcs.key -out ${CERT_PATH}/whmcs.csr -subj "/C=ID/O=DEV/OU=IT/CN=${WHMCS_SERVER_NAME}"
-    cp ${CERT_PATH}/whmcs.key ${CERT_PATH}/whmcs.key.org
-    openssl rsa -in ${CERT_PATH}/whmcs.key.org -passin file:${CERT_PATH}/whmcs_passphrase.txt -out ${CERT_PATH}/whmcs.key
-    openssl x509 -req -days 3650 -in ${CERT_PATH}/whmcs.csr -signkey ${CERT_PATH}/whmcs.key -out ${CERT_PATH}/whmcs.crt
-    export WHMCS_CERT_FILE="${CERT_PATH}/whmcs.crt"
-    export WHMCS_CERT_KEY_FILE="${CERT_PATH}/whmcs.key"
+if [ ! -f "${WHMCS_CERT_FILE}" ]; then
+    if [ ! -w "${CERT_PATH}" ]; then 
+        WHMCS_CERT_PATH=/tmp
+    else
+        WHMCS_CERT_PATH=${CERT_PATH}
+    fi
+    openssl rand -base64 32 > ${WHMCS_CERT_PATH}/whmcs_passphrase.txt
+    openssl genrsa -aes256 -passout file:${WHMCS_CERT_PATH}/whmcs_passphrase.txt -out ${WHMCS_CERT_PATH}/whmcs.key 2048
+    openssl req -new -passin file:${WHMCS_CERT_PATH}/whmcs_passphrase.txt -key ${WHMCS_CERT_PATH}/whmcs.key -out ${WHMCS_CERT_PATH}/whmcs.csr -subj "/C=ID/O=DEV/OU=IT/CN=${WHMCS_SERVER_NAME}"
+    cp ${WHMCS_CERT_PATH}/whmcs.key ${WHMCS_CERT_PATH}/whmcs.key.org
+    openssl rsa -in ${WHMCS_CERT_PATH}/whmcs.key.org -passin file:${WHMCS_CERT_PATH}/whmcs_passphrase.txt -out ${WHMCS_CERT_PATH}/whmcs.key
+    openssl x509 -req -days 3650 -in ${WHMCS_CERT_PATH}/whmcs.csr -signkey ${WHMCS_CERT_PATH}/whmcs.key -out ${WHMCS_CERT_PATH}/whmcs.crt
+    export WHMCS_CERT_FILE="${WHMCS_CERT_PATH}/whmcs.crt"
+    export WHMCS_CERT_KEY_FILE="${WHMCS_CERT_PATH}/whmcs.key"
 fi
 
 dockerize -template /app/default.tmpl > /etc/nginx/conf.d/default.conf
