ba967bf068a58ed7b7b1c96d0e453ea66b0506a8
Wiryono Lauw <wiryonolau@gmail.com>
2020-08-26 20:00:52 +0700

    Upgrade nginx conf and certificate script. Added phpmyadmin

--- docker/nginx/docker-entrypoint.sh
+++ docker/nginx/docker-entrypoint.sh
@@ -2,35 +2,43 @@
 mkdir -p ${CERT_PATH}
 
 if [[ ! -w "${CERT_PATH}" ]]; then 
-    ROOT_CERT_PATH=/tmp
+    ROOT_CERT_PATH="/root/"
 else
     ROOT_CERT_PATH=${CERT_PATH}
 fi
 
-if [[ ! -f "${WORDPRESS_CERT_PATH}" ]]; then
-    openssl rand -base64 32 > ${ROOT_CERT_PATH}/wp_passphrase.txt
-    openssl genrsa -aes256 -passout file:${ROOT_CERT_PATH}/wp_passphrase.txt -out ${ROOT_CERT_PATH}/wp.key 2048
-    openssl req -new -passin file:${ROOT_CERT_PATH}/wp_passphrase.txt -key ${ROOT_CERT_PATH}/wp.key -out ${ROOT_CERT_PATH}/wp.csr -subj "/C=ID/O=DEV/OU=IT/CN=${WORDPRESS_SERVER_NAME}"
-    cp ${ROOT_CERT_PATH}/wp.key ${ROOT_CERT_PATH}/wp.key.org
-    openssl rsa -in ${ROOT_CERT_PATH}/wp.key.org -passin file:${ROOT_CERT_PATH}/wp_passphrase.txt -out ${ROOT_CERT_PATH}/wp.key
-    openssl x509 -req -days 3650 -in ${ROOT_CERT_PATH}/wp.csr -signkey ${ROOT_CERT_PATH}/wp.key -out ${ROOT_CERT_PATH}/wp.crt
-    export WORDPRESS_CERT_PATH="${ROOT_CERT_PATH}/wp.crt"
-    export WORDPRESS_CERT_KEY_PATH="${ROOT_CERT_PATH}/wp.key"
-fi
+PASSPHRASE_FILE="${ROOT_CERT_PATH}/passphrase.txt"
+CERT_ENVS=$(env | grep -E "*_CERT_PATH")
+while read -r CERT_ENV; do
+    CERT_FILE=$(echo "${CERT_ENV}" | cut -f2 -d "=")
+    if [[ ! -f "${CERT_FILE}" ]]; then
+        CERT_NAME=$(echo "${CERT_ENV}" | cut -f1 -d "=" )
+        APP_NAME="${CERT_NAME%%_CERT_PATH}"
+        APP_SERVER_NAME_ENV="${APP_NAME}_SERVER_NAME"
+        APP_SERVER_NAME=$(echo "${!APP_SERVER_NAME_ENV}" | cut -f1 -d " " )
 
-if [[ ! -f "${WHMCS_CERT_PATH}" ]]; then
-    openssl rand -base64 32 > ${ROOT_CERT_PATH}/whmcs_passphrase.txt
-    openssl genrsa -aes256 -passout file:${ROOT_CERT_PATH}/whmcs_passphrase.txt -out ${ROOT_CERT_PATH}/whmcs.key 2048
-    openssl req -new -passin file:${ROOT_CERT_PATH}/whmcs_passphrase.txt -key ${ROOT_CERT_PATH}/whmcs.key -out ${ROOT_CERT_PATH}/whmcs.csr -subj "/C=ID/O=DEV/OU=IT/CN=${WHMCS_SERVER_NAME}"
-    cp ${ROOT_CERT_PATH}/whmcs.key ${ROOT_CERT_PATH}/whmcs.key.org
-    openssl rsa -in ${ROOT_CERT_PATH}/whmcs.key.org -passin file:${ROOT_CERT_PATH}/whmcs_passphrase.txt -out ${ROOT_CERT_PATH}/whmcs.key
-    openssl x509 -req -days 3650 -in ${ROOT_CERT_PATH}/whmcs.csr -signkey ${ROOT_CERT_PATH}/whmcs.key -out ${ROOT_CERT_PATH}/whmcs.crt
-    export WHMCS_CERT_PATH="${ROOT_CERT_PATH}/whmcs.crt"
-    export WHMCS_CERT_KEY_PATH="${ROOT_CERT_PATH}/whmcs.key"
-fi
+        printf "\n==============================================================================\n"
+        printf "Generate Self Signed Certificate for ${APP_SERVER_NAME}\n\n"
+        if [[ ! -f "${ROOT_CERT_PATH}/passphrase.txt" ]]; then
+            openssl rand -base64 32 > "${PASSPHRASE_FILE}"
+        fi
 
-dockerize -template /app/default.tmpl > /etc/nginx/conf.d/default.conf
-dockerize -template /app/whmcs.tmpl > /etc/nginx/conf.d/whmcs.conf
-dockerize -template /app/wordpress.tmpl > /etc/nginx/conf.d/wordpress.conf
+        openssl genrsa -aes256 -passout file:"${PASSPHRASE_FILE}" -out "${ROOT_CERT_PATH}/${APP_SERVER_NAME}.key" 2048
+        openssl req -new -passin file:"${PASSPHRASE_FILE}" -key "${ROOT_CERT_PATH}/${APP_SERVER_NAME}.key" -out "${ROOT_CERT_PATH}/${APP_SERVER_NAME}.csr" -subj "/C=ID/O=DEV/OU=IT/CN=${APP_SERVER_NAME}"
+        cp "${ROOT_CERT_PATH}/${APP_SERVER_NAME}.key" "${ROOT_CERT_PATH}/${APP_SERVER_NAME}.key.org"
+        openssl rsa -in "${ROOT_CERT_PATH}/${APP_SERVER_NAME}.key.org" -passin file:"${PASSPHRASE_FILE}" -out "${ROOT_CERT_PATH}/${APP_SERVER_NAME}.key"
+        openssl x509 -req -days 3650 -in "${ROOT_CERT_PATH}/${APP_SERVER_NAME}.csr" -signkey "${ROOT_CERT_PATH}/${APP_SERVER_NAME}.key" -out "${ROOT_CERT_PATH}/${APP_SERVER_NAME}.crt"
+        export ${APP_NAME}_CERT_PATH="${ROOT_CERT_PATH}/${APP_SERVER_NAME}.crt"
+        export ${APP_NAME}_CERT_KEY_PATH="${ROOT_CERT_PATH}/${APP_SERVER_NAME}.key"
+        printf "==============================================================================\n"
+    fi
+done <<< "${CERT_ENVS}"
+
+TMPL_FILE=./app/*.tmpl
+for TMPL in ${TMPL_FILE}
+do
+    FILE_NAME="${TMPL##*/}"
+    dockerize -template "${TMPL}" > "/etc/nginx/conf.d/${FILE_NAME%.*}.conf"
+done
 
 exec "$@"
